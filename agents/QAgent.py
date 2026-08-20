efc8dbbbda29dc86008193ed746984c2e3c418d7
Theo Smith <tls54@outlook.com>
2026-08-20 17:17:44 +0100

    Added update step for QAgent

--- agents/QAgent.py
+++ agents/QAgent.py
@@ -1,12 +1,19 @@
+import pickle
 import random
 
 class QAgent:
-    def __init__(self, q_table=None):
+    def __init__(self, q_table=None, alpha=0.1, gamma=0.9):
         self.q_table = q_table if q_table else {}
+        self.alpha = alpha
+        self.gamma = gamma
 
-    def save(self, path): pass
-    
-    def load(self, path): pass
+    def save(self, path):
+        with open(path, "wb") as f:
+            pickle.dump(self.q_table, f)
+
+    def load(self, path):
+        with open(path, "rb") as f:
+            self.q_table = pickle.load(f)
 
 
     def choose_action(self, state, legal_actions, epsilon=0.0):
@@ -18,14 +25,32 @@ class QAgent:
         values = {a: action_values.get(a, 0.0) for a in legal_actions}
         best = max(values.values())
         choices = [a for a, v in values.items() if v == best]
-        
+
         return random.choice(choices)
         
 
 
 
     def update(self, state, action, next_legal_action, reward, next_state, done):
-        pass
+        key = tuple(int(x) for x in state)
+        current_estimate = self.q_table.get(key, {}).get(action, 0.0)
+
+        if not done: 
+            next_key = tuple(int(x) for x in next_state)
+            next_action_values = self.q_table.get(next_key, {})
+            values = {a: next_action_values.get(a, 0.0) for a in next_legal_action}
+
+            best_next = max(values.values())
+
+            target = reward + self.gamma * best_next
+
+        
+        else:
+            target = reward
+
+        new_estimate = current_estimate + self.alpha * (target - current_estimate)
+
+        self.q_table.setdefault(key, {})[action] = new_estimate
 
 
 # Dummy table, just to see the shape:
@@ -44,10 +69,5 @@ dummy_q_table = {
         7: 0.15,
     },
 }
-
-state = (0, 0, 0, 0, 0, 0, 0, 0, 0)
-
-key = tuple(int(x) for x in state)
-action_values = dummy_q_table.get(key, {})
 
 
