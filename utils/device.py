2b66d8ad2b8bc3327ac32f0708f847f39d5750e1
Theo Smith <tls54@outlook.com>
2026-08-24 22:03:40 +0100

    add device util

---
+++ utils/device.py
@@ -0,0 +1,12 @@
+import torch
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    if torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
+
+
+if __name__ == '__main__':
+    print(get_device())
